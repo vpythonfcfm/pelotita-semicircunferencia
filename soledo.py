03ba262360408aa1c0ad0a708390bc6163a70df1
Martin <martinbataille@gmail.com>
2018-04-25 12:17:44 -0300

    Explicacion soledo

--- soledo.py
+++ soledo.py
@@ -9,4 +9,22 @@ def ec(y, t, m, g, r):
 
 
 def soledo(y0, t, m, g, r):
+	'''
+	Resuelve la ecuacion diferencial de la pelotita y devuelve
+	theta para cada instante t
+	y0: vector de condiciones iniciales: [theta0, thetapunto0]
+	t: vector con el tiempo (se recomienda numpy.linspace)
+	m: masa de la pelotita
+	g: aceleracion de gravedad
+	r: radio de la semiesfera
+
+	Ejemplo: theta = soledo([0.001,0],
+							np.linspace(0,10,101),
+							1,
+							9.8,
+							1)
+	Nota: si las condiciones iniciales son [0,0], la pelotita
+	no se va a mover, quedara en reposo sobre la punta de la
+	semiesfera
+	'''
 	return odeint(ec, y0, t, args=(m,g,r))[:,0]
